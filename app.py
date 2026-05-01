258f91a196d532447047e4d56961f8d006d19226
wackywaynesa-blip <wackywaynesa@gmail.com>
2026-05-01 17:24:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def verify():
     return "Verification failed", 403
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
